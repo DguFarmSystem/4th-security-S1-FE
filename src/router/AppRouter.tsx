9489eba07fdb293f663adc52a4efd7b6edc2374f
clwmfksek <clwmfksek@gmail.com>
2025-05-14 13:40:29 +0900

    refactor : file construct edit

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./index";
+
+const queryClient = new QueryClient();
+
+const AppRouter = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+};
+
+export default AppRouter;
