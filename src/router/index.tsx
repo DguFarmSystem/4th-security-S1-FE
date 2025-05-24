631eda243778360ae1e73e3b3b9fd8cf71d1a7fa
clwmfksek <clwmfksek@gmail.com>
2025-05-24 17:56:31 +0900

    Revert "update: 기본 틀 잡기기"
    This reverts commit 72161560885e7318a495f008574a6e916c0b44b1.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import Auth from "../pages/Auth";
 import MainPage from "../pages/MainPage";
+import Mypage from "../pages/Mypage";
 
 export const router = createBrowserRouter([
   {
@@ -11,5 +12,9 @@ export const router = createBrowserRouter([
   {
     path: "/main",
     element: <MainPage />,
+  },
+  {
+    path: "/mypage",
+    element: <Mypage />,
   },
 ]);
